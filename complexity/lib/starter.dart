73383e31cc6b120609ec94654178fd39da264c26
Kolawole Paul Oluwafemi <pauljacksonjr@Pauls-MacBook-Pro.local>
2022-05-20 10:42:29 +0100

    challenged solved - 1&2

--- complexity/lib/starter.dart
+++ complexity/lib/starter.dart
@@ -3,9 +3,13 @@ import 'package:complexity/stack.dart';
 void main() {
   // doings();
   const list = ['S', 'M', 'O', 'K', 'E'];
-  final smokeStack = Stack.of(list);
-  print(smokeStack);
-  smokeStack.pop();
+
+  // final smokeStack = Stack.of(list);
+  // print(smokeStack);
+  // smokeStack.pop();
+
+  print(balancedParenthesis('h((e))llo(world)()'));
+  print(reversedList(list));
 }
 
 final stack = Stack<int>();
@@ -21,3 +25,32 @@ doings() {
   final element = stack.pop();
   print("popped: $element");
 }
+
+String balancedParenthesis(String value) {
+  if (value.isEmpty) {
+    return "Empty value";
+  }
+
+  var startParenthesis = 0, endParenthesis = 0;
+  for (int i = 0; i < value.length; i++) {
+    if (value[i] == "(") {
+      startParenthesis += 1;
+    } else if (value[i] == ")") {
+      endParenthesis += 1;
+    }
+  }
+
+  print(
+      "startParenthesis: $startParenthesis - endParenthesis: $endParenthesis");
+
+  if (startParenthesis == endParenthesis) {
+    return "balanced parentheses";
+  } else {
+    return "unbalanced parentheses";
+  }
+}
+
+Iterable<dynamic> reversedList(List list) {
+  return list.reversed;
+}
+
